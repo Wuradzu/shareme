3ac7a7976c2af733c30dd256392b08a29b46007b
Wuradzu <savinykhvladislav@gmail.com>
2022-12-27 19:29:47 +0200

    WIP createPin

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -100,6 +100,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                         href={destination}
                         target="_blank"
                         rel="noreferrer"
+                        onClick={(e)=> e.stopPropagation()}
                         className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
                         >
                             <BsFillArrowUpRightCircleFill/>
