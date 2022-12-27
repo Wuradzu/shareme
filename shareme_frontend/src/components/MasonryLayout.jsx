b61668f3d568dddd1473c263f756b1ad8feaac48
Wuradzu <savinykhvladislav@gmail.com>
2022-12-27 17:21:49 +0200

    realized Pin component

--- shareme_frontend/src/components/MasonryLayout.jsx
+++ shareme_frontend/src/components/MasonryLayout.jsx
@@ -4,15 +4,15 @@ import Pin from './Pin'
 
 const breakpointObj = {
   default: 4,
-  3000: 6,
-  2000: 5,
+  3000: 5,
+  2000: 4,
   1200: 3,
   1000: 2,
   500: 1
 }
 
 const MasonryLayout = ({pins}) => {
-
+  
   return (
     <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj}>
       {pins?.map((pin)=>{
